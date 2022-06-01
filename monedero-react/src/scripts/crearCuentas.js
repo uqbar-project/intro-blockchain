eb56d24f4e451bba852db1b9126fb6a996ad3730
Fernando Dodino <fernando.dodino@gmail.com>
2022-06-01 07:48:31 -0300

    Fix build & script

--- monedero-react/src/scripts/crearCuentas.js
+++ monedero-react/src/scripts/crearCuentas.js
@@ -1,4 +1,16 @@
-import { authContract, smartContractAccount } from "../services/web3Wrapper.js"
+import Web3 from 'web3'
+
+// hay que usar el puerto y host que tiene truffle-config.js
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+
+// Se obtiene de ingresar a `truffle console` y pedirle `JSON.stringify(Wallet.abi)`
+const authABI = [{"constant":true,"inputs":[{"internalType":"string","name":"","type":"string"}],"name":"users","outputs":[{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"password","type":"string"},{"internalType":"address","name":"wallet_address","type":"address"},{"internalType":"bool","name":"existing","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"components":[{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"password","type":"string"},{"internalType":"address","name":"wallet_address","type":"address"},{"internalType":"bool","name":"existing","type":"bool"}],"internalType":"struct Auth.User","name":"user","type":"tuple"}],"name":"register","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"password","type":"string"}],"name":"login","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"string","name":"name","type":"string"}],"name":"exists","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"string","name":"name","type":"string"}],"name":"get_address","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]
+
+const smartContractAccount = '0xdCcD91A92D1d21D98D237545e6608cC364199fdd'
+
+const authContract = new web3
+  .eth
+  .Contract(authABI, '0x7bFb771b19Df2f5f97EeAae65C61713Bd1069e6A')
 
 // Hay que sacarlas de Ganache
 export const users = [
@@ -18,9 +30,9 @@ export const users = [
       wallet_address: '0x8C99F0689008471A43545b713f0c9a44dD0eE295',
   },
   {
-    name: 'jorgito',
+    name: 'jorge',
     password: '123',
-    wallet_address: '0x0904d75a810a06CD953774ffFAEc46b7B5bEF6bc',
+    wallet_address: '0xAac540961350fE462cc7322c54262169dB448b8e',
 },
 ]
 
