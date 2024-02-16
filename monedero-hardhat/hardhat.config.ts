5838dc46b2d7ffe504407c34f10f6d17fce6a1c1
Fernando Dodino <fernando.dodino@gmail.com>
2024-02-16 00:32:53 -0300

    Adding json-summary reporter

--- monedero-hardhat/hardhat.config.ts
+++ monedero-hardhat/hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox-viem'
 import '@nomicfoundation/hardhat-ignition-viem'
 import 'hardhat-abi-exporter'
+import 'solidity-coverage'
 
 const config: HardhatUserConfig = {
   solidity: '0.8.24',
---
+++ monedero-hardhat/solcover.ts
@@ -0,0 +1,3 @@
+module.exports = {
+  istanbulReporter: ['json', 'json-summary']
+}
